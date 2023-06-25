6d25fc42734475907c6ece706d681759153b3f07
0315akash <124701907+0315akash@users.noreply.github.com>
2023-06-25 13:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
+	
   //Complete the function
+	let table = {
+		"2":"abc",
+		"3":"def",
+		"4":"ghi",
+		"5":"jkl",
+		"6":"mno",
+		"7":"pqrs",
+		"8":"tuv",
+		"9":"wxyz",
+	}
+	let solutions =[];
+	function solve(currentIndex,calculatedString){
+		if(input.length <= currentIndex){
+			solutions.push(calculatedString);
+			return;
+		}
+		let currentChar = input[currentIndex]
+		for(let i=0;i<table[currentChar].length; i++){
+			solve(currentIndex+1,calculatedString+table[currentChar])
+		}
+	}
+	solve(0,"")
+	return solutions;
 }
 
 module.exports = letterCombinations;
